f413dac0fbe251944c93ec2da8507eda8e74be80
Pascal Goffin <ppjgoffin@gmail.com>
2017-11-06 14:27:57 -0700

    small stuff

--- aqandu/__init__.py
+++ aqandu/__init__.py
@@ -14,7 +14,7 @@
 
 import logging
 
-from flask import current_app, Flask, redirect, url_for, render_template
+from flask import Flask, render_template
 
 
 def create_app(config, debug=False, testing=False, config_overrides=None):
@@ -31,19 +31,9 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
     if not app.testing:
         logging.basicConfig(level=logging.INFO)
 
-    # # Setup the data model.
-    # with app.app_context():
-    #     model = get_model()
-    #     model.init_app(app)
-
-    # # Register the Bookshelf CRUD blueprint.
-    # from .crud import crud
-    # app.register_blueprint(crud, url_prefix='/books')
-
     # Add a default root route.
     @app.route("/")
     def index():
-        # return redirect(url_for('crud.list'))
         return render_template('main.html')
 
     @app.route("/request_sensor")
@@ -82,22 +72,3 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
         """.format(e), 500
 
     return app
-
-
-# def get_model():
-#     model_backend = current_app.config['DATA_BACKEND']
-#     if model_backend == 'cloudsql':
-#         from . import model_cloudsql
-#         model = model_cloudsql
-#     elif model_backend == 'datastore':
-#         from . import model_datastore
-#         model = model_datastore
-#     elif model_backend == 'mongodb':
-#         from . import model_mongodb
-#         model = model_mongodb
-#     else:
-#         raise ValueError(
-#             "No appropriate databackend configured. "
-#             "Please specify datastore, cloudsql, or mongodb")
-#
-#     return model
