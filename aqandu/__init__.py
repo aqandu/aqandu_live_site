5f74e9f0d21ee2fd48d8bdf55a671d9a9701cca9
JackWilb <jackwilburn@tutanota.com>
2020-01-23 17:04:03 -0700

    update the routes, remove some spaces and use better format string

--- aqandu/__init__.py
+++ aqandu/__init__.py
@@ -42,42 +42,34 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
 
     @app.route("/team")
     def team():
-
         return render_template('team.html')
 
     @app.route("/request_sensor")
     def request_sensor():
-
         return render_template('request_sensor.html')
 
     @app.route("/airu_sensor")
     def airu_sensor():
-
         return render_template('airu_sensor.html')
 
     @app.route("/project")
     def project():
-
         return render_template('project.html')
 
     @app.route("/newsroom")
     def newsroom():
-
         return render_template('newsroom.html')
 
     @app.route("/mailinglist")
     def mailinglist():
-
         return render_template('mailinglist.html')
 
     @app.route("/sensor_FAQ")
     def sensor_FAQ():
-
         return render_template('sensor_FAQ.html')
 
     @app.route("/about")
     def about():
-
         return render_template('about.html')
 
     # Add an error handler. This is useful for debugging the live application,
@@ -85,9 +77,6 @@ def create_app(config, debug=False, testing=False, config_overrides=None):
     # applications.
     @app.errorhandler(500)
     def server_error(e):
-        return """
-        An internal error occurred: <pre>{}</pre>
-        See logs for full stacktrace.
-        """.format(e), 500
+        return f"An internal error occurred: <pre>${e}</pre>. See logs for full stacktrace.".format(e), 500
 
     return app
